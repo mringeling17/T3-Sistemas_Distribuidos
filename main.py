f1981ab23531e13c47fa26f84beccfa62192afeb
mringeling17 <matias.ringeling@mail.udp.cl>
2022-11-15 22:41:40 -0300

    file names with numbers from 1 to len()

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import wikipedia as wiki
 
 artists = ["Bad Bunny", "Dua Lipa", "Ariana Grande", "Karol_G", "The Weeknd", "Justin Bieber", "Post Malone", "Ed Sheeran", "Taylor Swift", "Bizarrap"]
 wiki.set_lang('es')
+count = 1
 for i in artists:
     a = wiki.page(i)
-    with open(i+'.txt', 'w', encoding='utf-8') as f:
+    with open(str(count) + '.txt', 'w', encoding='utf-8') as f:
         f.write(a.content)
+    count += 1
 
