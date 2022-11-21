73f375f6ed86fc106d4ab8b80aad77c05c80499b
Gonzalo Salazar Muñoz <gonsalazarm@gmail.com>
2022-11-21 01:25:41 -0300

    añadida separación en carpetas

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import wikipedia as wiki
+import os
 
 artists = ["Bad Bunny", "Dua Lipa", "Ariana Grande", "Karol_G", "The Weeknd", "Justin Bieber", "Post Malone", "Ed Sheeran", "Taylor Swift", "Bizarrap"]
 wiki.set_lang('es')
 count = 1
+folder = "./carpeta1/"
+if not os.path.exists(folder):
+    os.makedirs(folder)
+    os.makedirs("./carpeta2/")
 for i in artists:
     a = wiki.page(i)
-    with open(str(count) + '.txt', 'w', encoding='utf-8') as f:
+    if count>5:
+        folder = "./carpeta2/"
+    with open(folder+str(count) + '.txt', 'w', encoding='utf-8') as f:
         f.write(a.content)
     count += 1
 
