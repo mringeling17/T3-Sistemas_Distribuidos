ba911911a63452891f9e14df338555955c0ef3c2
Gonzalo Salazar Muñoz <gonsalazarm@gmail.com>
2022-11-25 18:05:00 -0300

    reemplazado psql por mongo, output insertado

--- hadoop/data/main.py
+++ hadoop/data/main.py
@@ -1,6 +1,13 @@
 import wikipedia as wiki
 import os
 import time
+import json
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://mongo:27017/', username='root', password='tarea3')
+db = client['wiki']
+collection = db['pages']    
+
 
 artists = ["Bad Bunny", "Dua Lipa", "Ariana Grande", "Karol_G", "The Weeknd", "Justin Bieber", "Post Malone", "Ed Sheeran", "Taylor Swift", "Bizarrap"]
 wiki.set_lang('es')
@@ -26,4 +33,11 @@ while True:
     print("Waiting for Hadoop output file...")
     if os.path.exists("./output/output.json"):
         print("FILE FOUND!")
+        with open("./output/output.json", 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        if isinstance(data, list):
+            collection.insert_many(data)
+        else:
+            collection.insert_one(data)
+        print("HADOOP DATA INSERTED INTO MONGODB")
         break
